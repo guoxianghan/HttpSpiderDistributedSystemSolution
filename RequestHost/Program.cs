80ad6ddb7fe7a58cb59b8a47be6557bdbfdc76d1
Guo <Guo@DESKTOP-D1CSST5>
2017-03-24 22:04:56 +0800

    修改配置文件

--- RequestHost/Program.cs
+++ RequestHost/Program.cs
@@ -11,13 +11,14 @@ namespace RequestHost
     {
         static void Main(string[] args)
         {
-            Uri[] uri =new Uri[1];
-            uri[0] = new Uri("tcp:localhost");
-            ServiceHost host = new ServiceHost(typeof(RequestService.RequestService), uri);
+            //Uri[] uri =new Uri[1];
+            //uri[0] = new Uri("tcp:localhost");
+            ServiceHost host = new ServiceHost(typeof(RequestService.RequestService));
             host.Opened += delegate
             {
                 Console.WriteLine("RequestContract已经启动！");
             };
+            //host.Opened += (x,y) => { };
             host.Open();
             Console.ReadKey();
         }
